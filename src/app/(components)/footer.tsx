a9f5c1ef74fa1cb6fe7d55153abd7afe1dd004c1
tigeryash <yashwanth.venkatesan@gmail.com>
2025-09-15 18:14:44 -0400

    footer

--- src/app/(components)/footer.tsx
+++ src/app/(components)/footer.tsx
@@ -1,43 +1,26 @@
-"use client";
-
-import useLinesAnimation from "@/hooks/useLinesAnimation";
 import Link from "next/link";
 
-const Footer = () => {
-  const ref = useLinesAnimation({
-    type: "lines",
-    duration: 1,
-    stagger: 0,
-    opacity: 0,
-    ease: "power3.out",
-    y: 100,
-    targets: ".footer-target",
-    delay: 1.5,
-  });
+
+export default function Footer() {
   return (
-    <footer
-      ref={ref}
-      className="lg:fixed flex flex-col sm:flex-row text-xs items-center bottom-0 left-0 right-0 justify-between py-2 px-5 bg-neutral-50"
-    >
-      <p className="footer-target">
+    <footer className="flex flex-col text-neutral-950 sm:flex-row items-center bottom-0 left-0 right-0 justify-between py-4 px-5">
+      <p>
         &copy; {new Date().getFullYear()} Forzhen Studios. All rights reserved.
       </p>
       <div className="flex space-x-4 mt-2 sm:mt-0">
         <Link
           href="/privacy"
-          className="footer-target hover:text-neutral-500 transition-colors"
+          className="text-sm hover:text-neutral-700 transition-colors"
         >
           Privacy Policy
         </Link>
         <Link
           href="/terms"
-          className="footer-target hover:text-neutral-500 transition-colors"
+          className="text-sm hover:text-neutral-700 transition-colors"
         >
           Terms of Service
         </Link>
       </div>
     </footer>
   );
-};
-
-export default Footer;
+}
