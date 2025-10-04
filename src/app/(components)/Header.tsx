4d121cc1b9dfd7587e3d9380fdde581b4f0097a2
Troyzhen <tropicboytroy@gmail.com>
2025-10-04 15:08:01 -0400

    chnaged colors of logo and about button to better fit overall style

--- src/app/(components)/Header.tsx
+++ src/app/(components)/Header.tsx
@@ -4,10 +4,10 @@ import { Circle } from "lucide-react";
 
 export default function Header() {
   return (
-    <header className="w-full fixed top-0 flex items-center justify-between px-5 py-4  bg-neutral-50 z-20 ">
+    <header className="w-full fixed top-0 flex items-center justify-between px-5 py-4  bg-none z-20 ">
       <Link href="/">
         <Image
-          src="/studio_logo_dark.png"
+          src="/studio_logo_white.png"
           alt="Forzhen Studios Logo"
           width={49}
           height={49}
@@ -20,10 +20,10 @@ export default function Header() {
       {/* About Us link */}
       <Link
         href="/about"
-        className="flex group items-center px-4 py-2 rounded-full text-neutral-950 border border-neutral-950
-         hover:bg-neutral-950 hover:text-neutral-50 transition-colors duration-300"
+        className="flex group items-center px-4 py-2 rounded-full text-neutral-50 border border-neutral-50
+         hover:bg-neutral-50 hover:text-neutral-950 transition-colors duration-300"
       >
-        <Circle className="h-2 w-2 inline-block mr-2 group-hover:fill-neutral-50" />
+        <Circle className="h-2 w-2 inline-block mr-2 duration-100 ease-in-out group-hover:fill-neutral-950" />
         About Us
       </Link>
     </header>
