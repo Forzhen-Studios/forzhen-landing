eefa95464f520973e1d125ba7b07cfc920306374
Troyzhen <tropicboytroy@gmail.com>
2025-10-06 11:58:42 -0400

    added linkedin url

--- src/constants/socialLinks.ts
+++ src/constants/socialLinks.ts
@@ -1,5 +1,5 @@
 export const socialLinks = [
-  { name: "LINKEDIN", url: "#" },
+  { name: "LINKEDIN", url: "https://linkedin.com/company/forzhen-studios" },
   { name: "INSTAGRAM", url: "https://instagram.com/forzhenstudios" },
   { name: "YOUTUBE", url: "https://youtube.com/@forzhenstudios" },
 ];
