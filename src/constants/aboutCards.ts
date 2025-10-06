5b3779811d6da88bf4ce262b5cf8b2e5a8c6189c
Troyzhen <tropicboytroy@gmail.com>
2025-10-06 11:59:21 -0400

    added constants for about cards

---
+++ src/constants/aboutCards.ts
@@ -0,0 +1,21 @@
+export const aboutCards = [
+  {
+    title: "VISION",
+    content: "Our mission is to empower individuals and organizations to achieve their goals through innovative technology solutions."
+  },
+  {
+    title: "CREATIVITY",
+    content: "Our vision is to be a global leader in technology solutions, recognized for our commitment to excellence and customer satisfaction."
+  },
+  {
+    title: "PERFORMANCE",
+    content: "We value integrity, collaboration, and continuous improvement in all aspects of our work."
+  }
+];
+
+// Card rotations - about page
+export const cardRotations = [
+  "rotate-2",
+  "-rotate-1",
+  "rotate-1",
+];
