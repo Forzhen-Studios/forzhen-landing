1fad4ccfa8bb3be46fdfdac95f460f7276d15170
Troyzhen <tropicboytroy@gmail.com>
2025-11-10 11:08:53 -0500

    remove lines of comment

--- src/stores/useAnimationStore.ts
+++ src/stores/useAnimationStore.ts
@@ -1,17 +1,3 @@
-// import { create } from "zustand";
-
-// type AnimationState = {
-//   exit: boolean;
-//   setExit: (exit: boolean) => void;
-// };
-
-// const useAnimationStore = create<AnimationState>((set) => ({
-//   exit: false,
-//   setExit: (exit: boolean) => set({ exit }),
-// }));
-
-// export default useAnimationStore;
-
 import { create } from "zustand";
 
 type AnimationState = {
